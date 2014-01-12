014f43500967854ea3680c8c885ad041d4299b02
Lorry Rocha <sorai120@yahoo.com>
2014-01-12 11:39:25 -0500

    I fixed the javascript timer box and rearranged the page layout a bit. Fun stuff.

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,12 +1,14 @@
   var totalSeconds = 30;
-  $( "#num_input" ).keyup(function(e) {
+
+
+  $( "#num_input" ).keypress(function(e) {
     if (e.keyCode == '13') {
+      e.preventDefault();
       setCustomNum($( "#num_input" ).val());
     }
-  })
+  });
 
   function setCustomNum(num) {
-    console.log(num)
     if (parseInt(num)) {
       totalSeconds = parseInt(num);
     }
@@ -26,7 +28,6 @@
     if (totalSeconds > 0) {
     window.setTimeout('Tick()', 1000);
     } else {
-      setCustomNum();
       location.reload();
       return;
     }

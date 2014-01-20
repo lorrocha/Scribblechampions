6f62140d3a9352ea03d0f4ab9703c6a4301935a4
Lorry Rocha <sorai120@yahoo.com>
2014-01-19 22:18:59 -0500

    Added config.assets.compile = true to production.rb to see if it works in Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,7 @@ Scribblechampion::Application.configure do
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
+  config.assets.compile = true
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
